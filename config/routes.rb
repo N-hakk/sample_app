2dd3ce93a85c0f9d9fd6755cbc5f5ce4cd1012b9
nhakkari <hakkari.nawfal@gmail.com>
2017-04-21 16:11:30 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   match '/help',    :to => 'pages#help', via: [:get] 
 
   root :to => 'pages#home'
-
-
-
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #end
 end
