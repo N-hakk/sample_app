3fdd433c7b4ee5318e283b669b68fded8f684109
nhakkari <hakkari.nawfal@gmail.com>
2017-04-24 09:58:25 +0200

    Fin premiere version de User model

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   match '/contact', :to => 'pages#contact' , via: [:get] 
   match '/about',   :to => 'pages#about', via: [:get] 
   match '/help',    :to => 'pages#help', via: [:get] 
-
+  get "users/show/:id",:to => 'users#show'
   root :to => 'pages#home'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #end
 end
